4e59a631e06358575a22ed95e3206c5244d9e4e9
Nate Carlin <carlin010@knights.gannon.edu>
2016-09-19 10:27:28 -0400

    Tested with iOS & Android

--- platforms/android/assets/www/js/boot.js
+++ platforms/android/assets/www/js/boot.js
@@ -1,7 +1,7 @@
-System.register(['angular2/platform/browser', './app.component', './HeaderComponent'], function(exports_1, context_1) {
+System.register(['angular2/platform/browser', './app.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    var browser_1, app_component_1, HeaderComponent_1;
+    var browser_1, app_component_1;
     return {
         setters:[
             function (browser_1_1) {
@@ -9,13 +9,9 @@ System.register(['angular2/platform/browser', './app.component', './HeaderCompon
             },
             function (app_component_1_1) {
                 app_component_1 = app_component_1_1;
-            },
-            function (HeaderComponent_1_1) {
-                HeaderComponent_1 = HeaderComponent_1_1;
             }],
         execute: function() {
             browser_1.bootstrap(app_component_1.AppComponent);
-            browser_1.bootstrap(HeaderComponent_1.HeaderComponent);
         }
     }
 });
