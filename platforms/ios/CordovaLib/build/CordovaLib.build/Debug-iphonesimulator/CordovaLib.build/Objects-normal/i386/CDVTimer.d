e0c278f6a65efe35046ce60458ac30f1d286b338
Niklas Fink <nudann@gmail.com>
2016-09-21 22:25:40 -0400

    removed platforms

--- platforms/ios/CordovaLib/build/CordovaLib.build/Debug-iphonesimulator/CordovaLib.build/Objects-normal/i386/CDVTimer.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/snowflake/Apps/class-survey/platforms/ios/CordovaLib/Classes/Public/CDVTimer.m \
-  /Users/snowflake/Apps/class-survey/platforms/ios/CordovaLib/Classes/Public/CDVTimer.h
