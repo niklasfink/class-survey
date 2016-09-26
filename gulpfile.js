b115de9b0800a49c33e46b32963578ca198d5f70
Nate Carlin <carlin010@knights.gannon.edu>
2016-09-26 17:55:38 -0400

    Add gulpfile tasks for Twitter Bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,16 @@ var gulp = require('gulp'),
   sass = require('gulp-sass'),
   minifyCss = require('gulp-minify-css'),
   rename = require('gulp-rename'),
+  header = require('gulp-header');
+  cleanCSS = require('gulp-clean-css');
+  rename = require("gulp-rename");
+  uglify = require('gulp-uglify');
+  pkg = require('./package.json');
   bower = require('bower'),
   sh = require('shelljs'),
   tscConfig = require('./tsconfig.json');
+  less = require('gulp-less');
+  browserSync = require('browser-sync').create();
 
 var dist = 'dist/',
   tsSrc = 'src/backend/typescript/',
@@ -21,8 +28,30 @@ var paths = {
   webSrc: ['src/webapp/']
 };
 
+// Set the banner content
+var banner = ['/*!\n',
+    ' * Class Survey - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+    ' * Copyright 2016-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
+    ' */\n',
+    ''
+].join('');
+
 gulp.task('default', ['install']);
 gulp.task('web', ['copylibs', 'typescript', 'html', 'css', 'watch', 'webserver']);
+// Dev task with browserSync
+gulp.task('dev', ['browserSync', 'less', 'minify-css', 'js', 'minify-js'], function() {
+    gulp.watch('less/*.less', ['less']);
+    gulp.watch('dist/css/*.css', ['minify-css']);
+    gulp.watch('js/*.js', ['minify-js']);
+    // Reloads the browser whenever HTML or JS files change
+    gulp.watch('pages/*.html', browserSync.reload);
+    gulp.watch('dist/js/*.js', browserSync.reload);
+});
+
+
+// Run everything
+gulp.task('bootstrap', ['minify-css', 'minify-js', 'copy']);
 
 gulp.task('sass', function (done) {
   return gulp.src('src/ionic/scss/app.scss')
@@ -39,14 +68,97 @@ gulp.task('sass', function (done) {
     .on('end', done);
 });
 
+gulp.task('less', function() {
+    return gulp.src('less/sb-admin-2.less')
+        .pipe(less())
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
 gulp.task('html', function () {
   return gulp.src(paths.webSrc + '**/*.html')
     .pipe(gulp.dest(paths.dist + ''));
 });
 
+// Copy JS to dist
+gulp.task('js', function() {
+    return gulp.src(['js/sb-admin-2.js'])
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+})
+
+// Minify JS
+gulp.task('minify-js', ['js'], function() {
+    return gulp.src('js/sb-admin-2.js')
+        .pipe(uglify())
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Copy vendor libraries from /bower_components into /vendor
+gulp.task('copy', function() {
+    gulp.src(['bower_components/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
+        .pipe(gulp.dest('vendor/bootstrap'))
+
+    gulp.src(['bower_components/bootstrap-social/*.css', 'bower_components/bootstrap-social/*.less', 'bower_components/bootstrap-social/*.scss'])
+        .pipe(gulp.dest('vendor/bootstrap-social'))
+
+    gulp.src(['bower_components/datatables/media/**/*'])
+        .pipe(gulp.dest('vendor/datatables'))
+
+    gulp.src(['bower_components/datatables-plugins/integration/bootstrap/3/*'])
+        .pipe(gulp.dest('vendor/datatables-plugins'))
+
+    gulp.src(['bower_components/datatables-responsive/css/*', 'bower_components/datatables-responsive/js/*'])
+        .pipe(gulp.dest('vendor/datatables-responsive'))
+
+    gulp.src(['bower_components/flot/*.js'])
+        .pipe(gulp.dest('vendor/flot'))
+
+    gulp.src(['bower_components/flot.tooltip/js/*.js'])
+        .pipe(gulp.dest('vendor/flot-tooltip'))
+
+    gulp.src(['bower_components/font-awesome/**/*', '!bower_components/font-awesome/*.json', '!bower_components/font-awesome/.*'])
+        .pipe(gulp.dest('vendor/font-awesome'))
+
+    gulp.src(['bower_components/jquery/dist/jquery.js', 'bower_components/jquery/dist/jquery.min.js'])
+        .pipe(gulp.dest('vendor/jquery'))
+
+    gulp.src(['bower_components/metisMenu/dist/*'])
+        .pipe(gulp.dest('vendor/metisMenu'))
+
+    gulp.src(['bower_components/morrisjs/*.js', 'bower_components/morrisjs/*.css', '!bower_components/morrisjs/Gruntfile.js'])
+        .pipe(gulp.dest('vendor/morrisjs'))
+
+    gulp.src(['bower_components/raphael/raphael.js', 'bower_components/raphael/raphael.min.js'])
+        .pipe(gulp.dest('vendor/raphael'))
+
+})
+
 gulp.task('css', function () {
   return gulp.src(paths.webSrc + '**/*.css')
     .pipe(gulp.dest(paths.dist + ''));
+});
+
+// Minify compiled CSS
+gulp.task('minify-css', ['less'], function() {
+    return gulp.src('dist/css/sb-admin-2.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
 gulp.task('copylibs', function () {
@@ -78,12 +190,22 @@ gulp.task('watch', function () {
   gulp.watch(paths.sass, ['sass']);
 });
 
+// Might not need this funciton when using browserSync
 gulp.task('webserver', function () {
   return gulp.src(paths.dist)
     .pipe(webserver({
       livereload: true
     }));
 });
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: ''
+        },
+    })
+})
 
 gulp.task('install', ['git-check'], function () {
   return bower.commands.install()
