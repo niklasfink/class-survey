db62e6e0bde530ef9fe07d992b6ee710031589d2
Niklas Fink <nudann@gmail.com>
2016-09-26 21:08:26 -0400

    Merge master into fb-feat

--- src/backend/typescript/app.component.ts
+++ src/backend/typescript/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from 'angular2/core';
 import {LoginComponent} from "./login.component";
 
 @Component({
-  selector: 'app',
+  selector: 'my-app',
   template: `
   <h1>Welcome to my App</h1>
   <login></login>
