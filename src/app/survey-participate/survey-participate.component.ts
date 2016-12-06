0412d5b2874f89e88b03227d8524356bc3a36e1b
Niklas Fink <nudann@gmail.com>
2016-12-06 18:13:51 -0500

    Participate page implemented and working with 3 types
    Just functional. Needs design

--- src/app/survey-participate/survey-participate.component.ts
+++ src/app/survey-participate/survey-participate.component.ts
@@ -1,6 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { GlobalService } from '../globals';
+import { FirebaseApp } from 'angularfire2';
+import { Observable } from 'rxjs';
+
+class Participation {
+  constructor(type: any) {
+    this.type = type;
+  }
+  type: number = -1;
+  answer: any = null;
+}
 
 @Component({
   selector: 'app-survey-participate',
@@ -10,25 +20,41 @@ import { GlobalService } from '../globals';
 export class SurveyParticipateComponent implements OnInit {
 
   private sub: any;
+  private sub2: any;
+  private sub3: any;
   private id: any;
   private survey: any;
-  private participation: any = [];
+  private participations: any = new Array<Participation>();
+  private storage: any;
+  private uploadpercentage: number = 0;
+  private imagePath: string;
+  private participationid: any;
+  private savePath: string;
+  private allUploaded: boolean = false;
+  @ViewChild('uploadform') input;
 
   constructor(
     private route: ActivatedRoute,
     private globals: GlobalService,
-    private router: Router
-  ) { }
+    private router: Router,
+    @Inject(FirebaseApp) firebase: any
+  ) {
+    this.participationid = firebase.database().ref().push().key;
+    this.storage = firebase.storage().ref();
+  }
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
       this.id = params['id'];
       if (this.id.length != 20)
         this.router.navigateByUrl("/");
-      this.sub = this.globals.af.database.object("/surveys/" + this.id).subscribe(survey => {
+      this.sub2 = this.globals.af.database.object("/surveys/" + this.id).subscribe(survey => {
         if (survey.$exists()) {
+          for (let i = 0; i < survey.questions.length; i++) {
+            this.participations[i] = new Participation(survey.questions[i].type);
+          }
           this.survey = survey;
-          console.log(survey);
+          this.imagePath = "/surveyimages/" + this.id;
         } else {
           this.router.navigateByUrl("/");
         }
@@ -36,8 +62,67 @@ export class SurveyParticipateComponent implements OnInit {
     });
   }
 
+  save() {
+    this.globals.af.database.object("/participations/" + this.id + "/" + this.participationid).set(this.participations).then(() => {
+      console.log("save successfull");
+      //forward to 'complete page'
+    }).catch(err => console.log(err));
+  }
+
+  upload(event, questionindex) {
+    let files = event.srcElement.files[0];
+    if (!files) return;
+    this.savePath = this.imagePath + "/" + this.participationid + "_" + files.name;
+    let task = this.storage.child(this.savePath).put(files);
+    task.on('state_changed', snapshot => {
+      this.uploadpercentage = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+    }, error => {
+      console.log(error);
+      switch (error.code) {
+        case 'storage/unauthorized':
+          // User doesn't have permission to access the object
+          break;
+        case 'storage/canceled':
+          // User canceled the upload
+          break;
+        case 'storage/unknown':
+          // Unknown error occurred (error.serverResponse)
+          break;
+      }
+    }, () => {
+      // Upload successfull
+      this.participations[questionindex].answer = task.snapshot.downloadURL;
+      this.participations.forEach(p => {
+        if (p.type >= 3) {
+          if (p.answer.length > 1) {
+            this.allUploaded = true;
+          } else {
+            this.allUploaded = false;
+            return;
+          }
+        }
+      });
+    });
+  }
+
+  removeImage(i) {
+    this.participations[i].answer = "";
+    this.input.nativeElement.value = "";
+    this.uploadpercentage = 0;
+    this.allUploaded = false;
+    this.storage.child(this.savePath).delete().then(() => {
+      console.log("deleted!");
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
   ngOnDestroy() {
     if (this.sub)
+      this.sub.unsubscribe();
+    if (this.sub2)
+      this.sub.unsubscribe();
+    if (this.sub3)
       this.sub.unsubscribe();
   }
 }
